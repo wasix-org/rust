8bd9b71f9ff4fb651843a927f3a8c07a0acebf29
Arshia Ghafoori <arshia001@live.com>
2024-08-06 19:35:23 +0400

    let wasm64 targets build

--- src/bootstrap/src/core/sanity.rs
+++ src/bootstrap/src/core/sanity.rs
@@ -223,7 +223,7 @@ than building it.
         }
 
         // We don't use a C compiler on wasm32
-        if target.contains("wasm32") {
+        if target.contains("wasm32") || target.contains("wasm64") {
             continue;
         }
 
