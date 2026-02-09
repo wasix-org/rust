9eb76a529b701298390685904b3429156a9d0916
Arshia <arshia001@live.com>
2026-02-09 19:38:17 +0400

    Add support for `std::fs::canonicalize`

--- library/std/src/sys/fs/mod.rs
+++ library/std/src/sys/fs/mod.rs
@@ -30,6 +30,9 @@ cfg_if::cfg_if! {
     } else if #[cfg(target_os = "uefi")] {
         mod uefi;
         use uefi as imp;
+    } else if #[cfg(all(target_os = "wasi", target_vendor = "wasmer"))] {
+        mod wasix;
+        use wasix as imp;
     } else if #[cfg(target_os = "wasi")] {
         mod wasi;
         use wasi as imp;
